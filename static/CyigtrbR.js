426b4f20cfdb33a3d2d87ba5ebf85f2dbc584366
Ana Ned <anastasiia.nedashkivska@weareuncapped.com>
2026-09-17 13:49:27 +0200

    Publish LoC offer V2 (2026-09-17)

---
+++ static/CyigtrbR.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-BVqqrrqN.js";const o=e=>t.createElement("svg",{width:24,height:24,viewBox:"0 0 24 24",fill:"currentColor",xmlns:"http://www.w3.org/2000/svg",...e},t.createElement("path",{d:"M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z"}));export{o as S};
