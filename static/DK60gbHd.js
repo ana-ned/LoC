5b6a67c50d9fb9249e83a57dcda0cb53b1752a40
Ana Ned <anastasiia.nedashkivska@weareuncapped.com>
2026-09-23 12:54:56 +0200

    Publish LoC offer V2 (2026-09-23)

---
+++ static/DK60gbHd.js
@@ -0,0 +1,1 @@
+import{cj as i,cF as s}from"./index-CWWEAMep.js";function h(n,o,a){const t=i(n,a?.in);if(isNaN(o))return s(n,NaN);if(!o)return t;const r=t.getDate(),e=s(n,t.getTime());e.setMonth(t.getMonth()+o+1,0);const c=e.getDate();return r>=c?e:(t.setFullYear(e.getFullYear(),e.getMonth(),r),t)}export{h as a};
