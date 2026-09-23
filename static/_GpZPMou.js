41eb33c693a0eb7bb7a0f84d86130177c93e203e
Ana Ned <anastasiia.nedashkivska@weareuncapped.com>
2026-09-23 16:29:29 +0200

    Publish LoC offer V2 (2026-09-23)

--- static/_GpZPMou.js
+++
@@ -1,1 +0,0 @@
-import{j as e,I as r}from"./index-TYCItr62.js";const a=({children:s,className:t})=>e.jsx("div",{className:r(t),children:s});a.Content=({children:s,className:t})=>e.jsx("div",{className:r(t),children:s});a.Footer=({children:s})=>e.jsx("div",{className:"mt-8 flex flex-col gap-y-4",children:s});export{a as F};
